1e3e948f2b7a3c28e64fee2c7dae916eed243a5a
Zaid Abdulla <zkam83@gmail.com>
2013-08-25 01:40:30 +0300

    Escape home page queries.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    online_threshold_green = (Time.now - (60 * 30))
+    online_threshold_green = (Time.now - (60 * 10))
     online_threshold_yellow = (Time.now - (60 * 60 * 24))
 
     @online_green_query = {'summary.lastInform' => {'$gt' => online_threshold_green}}
@@ -8,14 +8,12 @@ class HomeController < ApplicationController
     @online_red_query = {'summary.lastInform' => {'$lt' => online_threshold_yellow}}
 
     @online_green_count, @online_yellow_count, @online_red_count = Rails.cache.fetch('online_count', :expires_in => 60.seconds) do
-      require 'net/http'
-      require 'json'
       http = Net::HTTP.new(Rails.configuration.genieacs_api_host, Rails.configuration.genieacs_api_port)
-      res = http.head("/devices/?query=#{ActiveSupport::JSON.encode(@online_green_query)}")
+      res = http.get("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_green_query)}")
       green = res['Total'].to_i
-      res = http.head("/devices/?query=#{ActiveSupport::JSON.encode(@online_yellow_query)}")
+      res = http.get("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_yellow_query)}")
       yellow = res['Total'].to_i
-      res = http.head("/devices/?query=#{ActiveSupport::JSON.encode(@online_red_query)}")
+      res = http.get("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_red_query)}")
       red = res['Total'].to_i
       [green, yellow, red]
     end
