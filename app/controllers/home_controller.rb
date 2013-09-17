d79f624869ede8d584a4e1a445983e9a01faae06
Zaid Abdulla <zkam83@gmail.com>
2013-09-17 14:14:41 +0300

    Use head request instead of get for home page graph.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,11 +10,11 @@ class HomeController < ApplicationController
 
     @online_green_count, @online_yellow_count, @online_red_count = Rails.cache.fetch('online_count', :expires_in => 60.seconds) do
       http = Net::HTTP.new(Rails.configuration.genieacs_api_host, Rails.configuration.genieacs_api_port)
-      res = http.get("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_green_query)}")
+      res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_green_query)}")
       green = res['Total'].to_i
-      res = http.get("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_yellow_query)}")
+      res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_yellow_query)}")
       yellow = res['Total'].to_i
-      res = http.get("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_red_query)}")
+      res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_red_query)}")
       red = res['Total'].to_i
       [green, yellow, red]
     end
