28eacb4f02df439bb4a3a488c974ce1b24c1c354
Zaid Abdulla <zkam83@gmail.com>
2013-12-17 13:40:46 +0300

    Home page graphs are now configurable.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,21 @@
 class HomeController < ApplicationController
   def index
-    require 'net/http'
-    online_threshold_green = (Time.now - (60 * 10))
-    online_threshold_yellow = (Time.now - (60 * 60 * 24))
-
-    @online_green_query = {'summary.lastInform' => {'$gt' => online_threshold_green}}
-    @online_yellow_query = {'summary.lastInform' => {'$gt' => online_threshold_yellow, '$lt' => online_threshold_green}}
-    @online_red_query = {'summary.lastInform' => {'$lt' => online_threshold_yellow}}
-
-    @online_green_count, @online_yellow_count, @online_red_count = Rails.cache.fetch('online_count', :expires_in => 60.seconds) do
+    @graphs = Rails.cache.fetch('graphs', :expires_in => 60.seconds) do
+      require 'net/http'
       http = Net::HTTP.new(Rails.configuration.genieacs_api_host, Rails.configuration.genieacs_api_port)
-      res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_green_query)}")
-      green = res['Total'].to_i
-      res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_yellow_query)}")
-      yellow = res['Total'].to_i
-      res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(@online_red_query)}")
-      red = res['Total'].to_i
-      [green, yellow, red]
+
+      graphs = ActiveSupport::JSON.decode(ERB.new(File.read('config/graphs.json.erb')).result)
+      for group_name, group_graphs in graphs
+        for graph_name, graph in group_graphs
+          for slice in graph
+            res = http.head("/devices/?query=#{CGI::escape ActiveSupport::JSON.encode(slice['query'])}")
+            slice['data'] = res['Total'].to_i
+            slice['href'] = url_for(controller: 'devices', action: 'index', params: {query: ActiveSupport::JSON.encode(slice['query'])})
+            slice.delete('query')
+          end
+        end
+      end
+      next graphs
     end
   end
 end
