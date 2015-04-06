6d6228ecc65be7fce00f42c32dcb3c592943ad89
Dan Morphis <dan.m@aptalaska.com>
2015-04-06 09:06:15 -0800

    Rename genieacs_api_scheme to genieacs_api_use_ssl per discussion.

--- lib/util.rb
+++ lib/util.rb
@@ -1,7 +1,7 @@
 
 def create_api_conn
   http = Net::HTTP.new(Rails.configuration.genieacs_api_host, Rails.configuration.genieacs_api_port)
-  http.use_ssl = Rails.configuration.genieacs_api_scheme == 'https'
+  http.use_ssl = Rails.configuration.genieacs_api_use_ssl
   #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
   return http
