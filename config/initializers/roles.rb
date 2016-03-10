c67aedcc515bdf54ec9aa4e3f22e25c1ae749e7e
Oliver Kraitschy <okraitschy@tdt.de>
2016-03-10 16:20:29 +0100

    Integrate db-based user- and rolemanagement in authentication infrastructure.

--- config/initializers/roles.rb
+++ config/initializers/roles.rb
@@ -1,2 +1,5 @@
-GenieacsGui::Application.config.permissions = YAML.load_file('config/roles.yml')
-GenieacsGui::Application.config.users = YAML.load_file('config/users.yml')
+case GenieacsGui::Application.config.auth_method
+when :yml
+  GenieacsGui::Application.config.permissions = YAML.load_file('config/roles.yml')
+  GenieacsGui::Application.config.users = YAML.load_file('config/users.yml')
+end
