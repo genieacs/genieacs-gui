909db6f65448114f526ef7acfa56b0ed6e0c5931
Zaid Abdulla <zaid@genieacs.com>
2016-09-15 22:21:31 -0700

    Remove references to summary parameter aliases

--- config/initializers/parameters.rb
+++ config/initializers/parameters.rb
@@ -1,7 +1,7 @@
 GenieacsGui::Application.config.summary_parameters = YAML.load_file('config/summary_parameters.yml')
 GenieacsGui::Application.config.index_parameters = YAML.load_file('config/index_parameters.yml')
 
-GenieacsGui::Application.config.device_filters = {'Last inform' => 'summary.lastInform', 'Tag' => '_tags'}
+GenieacsGui::Application.config.device_filters = {'Last inform' => '_lastInform', 'Tag' => '_tags'}
 GenieacsGui::Application.config.device_filters.merge!(GenieacsGui::Application.config.summary_parameters)
 GenieacsGui::Application.config.device_filters.merge!(GenieacsGui::Application.config.index_parameters)
 GenieacsGui::Application.config.device_filters.delete_if {|key, value| not value.is_a?(String)}
@@ -12,4 +12,3 @@ end
 GenieacsGui::Application.config.parameter_renderers = YAML.load_file('config/parameter_renderers.yml')
 
 GenieacsGui::Application.config.parameters_edit = YAML.load_file('config/parameters_edit.yml')
-
