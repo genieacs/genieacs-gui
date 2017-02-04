a97b9561446a2b78179298c08a8ef61e0b957d5e
DrumSergio <sergio1993_1@hotmail.com>
2017-02-04 13:35:22 +0100

    Add session expiry time to 30min

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_genieacs-gui_session'
+Rails.application.config.session_store :cookie_store,
+  key: '_genieacs-gui_session',
+  expire_after: 30.minutes
