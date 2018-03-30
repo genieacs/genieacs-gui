2c3f2d7e0cb4e1656e7276261704841c5b8db3ff
Theeraphat Jantakat <pppangpondpp7@gmail.com>
2018-03-30 15:13:35 +0700

    Added filter start date and end date to log activity (#11)
    * Added filter start date and end date to log activity
    
    * Used  present? instead to blank?

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,11 +3,21 @@ class LogsController < ApplicationController
     can?(:read, 'logs') do
       off = params.include?(:page) ? (Integer(params[:page]) - 1) * Rails.configuration.page_size : 0
       lim = Rails.configuration.page_size
-      @logs = PaperTrail::Version.order(created_at: :desc)
+      @logs = PaperTrail::Version
 
+      @filter_from = params[:from]
+      @filter_to = params[:to]
+      if @filter_start.present? && @filter_to.present?
+        @logs = @logs.where('created_at BETWEEN ? AND ?', @filter_start, @filter_to)
+      elsif @filter_start.present?
+        @logs = @logs.where('created_at > ?', @filter_start)
+      elsif @filter_to.present?
+        @logs = @logs.where('created_at < ?', @filter_to)
+      end
+
+      @logs = @logs.order(created_at: :desc)
       @total = @logs.count
       @logs = @logs.limit(lim).offset(off)
     end
-
   end
 end
