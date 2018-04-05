8a88ef80197bb39797cf4e0dfa5d36303e206976
Theeraphat Jantakat <pppangpondpp7@gmail.com>
2018-04-05 13:39:50 +0700

    Changed add privilege to dropdown (#22)
    * Changed privilege to dropdown
    
    * Improved privilege of admin and super user in data seed
    
    * Changed scope to constant

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -1,6 +1,32 @@
 class Privilege < ApplicationRecord
   has_paper_trail
 
+  ACTION_COLLECTION = [
+    ['Read', 'read'], ['Create', 'create'], ['Update', 'update'], ['Delete', 'delete']
+  ]
+  WEIGHT_COLLECTION = [['Allow', 1], ['Deny', -1]]
+  RESOURCE_COLLECTION = ['/',
+                         '/home',
+                         '/devices',
+                         '/devices/{id}',
+                         '/devices/tags',
+                         '/devices/reboot',
+                         '/devices/factory_reset',
+                         '/devices/download',
+                         '/devices/files',
+                         '/tasks',
+                         '/tasks/retry',
+                         '/presets',
+                         '/faults',
+                         '/objects',
+                         '/previsions',
+                         '/provisions',
+                         '/virtaul_parameters',
+                         '/files',
+                         '/logs',
+                         '/users',
+                         '/roles']
+
   belongs_to :role
   validates :action, presence: true,
                     length: { minimum: 4 }
@@ -8,4 +34,5 @@ class Privilege < ApplicationRecord
                     length: { minimum: 1 }
   validates :resource, presence: true,
                     length: { minimum: 1 }
+
 end
