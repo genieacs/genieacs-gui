051266dabb082e7e7b0c50761de366dab76adfd3
Oliver Kraitschy <ok@dev.tdt.de>
2018-11-08 10:58:32 +0100

    Clear db cache on startup

--- config/initializers/roles.rb
+++ config/initializers/roles.rb
@@ -1,4 +1,6 @@
 case GenieacsGui::Application.config.auth_method
+when :db
+  GenieacsGui::ApplicationController.new.clear_permissions_cache
 when :yml
   GenieacsGui::Application.config.permissions = YAML.load_file('config/roles.yml')
   GenieacsGui::Application.config.users = YAML.load_file('config/users.yml')
