a4a61f33cd652eb1d603dbb6d0b091cd5ce1b2d1
Zaid Abdulla <zaid@genieacs.com>
2019-02-28 23:23:10 -0800

    Fix double url unescape in search

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -66,7 +66,7 @@ class DevicesController < ApplicationController
 
       @query = {}
       if params.has_key?('query')
-        @query = ActiveSupport::JSON.decode(URI.unescape(params['query']))
+        @query = ActiveSupport::JSON.decode(params['query'])
       end
       if request.format == Mime::CSV
         @devices = find_devices(@query, 0, 0)
